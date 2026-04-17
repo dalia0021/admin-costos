1274748086540669dc567974e6a4413188a8164e
dalia0021 <dalia-0021@hotmail.com>
2026-04-17 16:41:19 -0700

    Update ViewProductos.vue

--- src/views/ViewProductos.vue
+++ src/views/ViewProductos.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+/*import { ref, onMounted } from 'vue'
 import { supabase } from '@src/lib/supabaseClient'
 
 const countries = ref([])
@@ -15,11 +15,11 @@ async function getCountries() {
 
 onMounted(() => {
   getCountries()
-})
+})*/
 </script>
 
 <template>
   <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.nombre }}</li>
+   <!-- <li v-for="country in countries" :key="country.id">{{ country.nombre }}</li>-->
   </ul>
 </template>
